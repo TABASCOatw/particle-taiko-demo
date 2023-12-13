23dab0eb4936cf1618dfc01a58d993fcecd202f3
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-12-12 17:24:41 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PageConnectKit from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { TaikoJolnir } from '@particle-network/chains';
+import { AuthCoreContextProvider } from '@particle-network/auth-core-modal';
+
+import App from './App';
 
 import('buffer').then(({ Buffer }) => {
   window.Buffer = Buffer;
@@ -8,6 +12,27 @@ import('buffer').then(({ Buffer }) => {
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <PageConnectKit />
+    <AuthCoreContextProvider
+      options={{
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        clientKey: process.env.REACT_APP_CLIENT_KEY,
+        appId: process.env.REACT_APP_APP_ID,
+        themeType: 'dark',
+        fiatCoin: 'USD',
+        language: 'en',
+        erc4337: {
+          name: "SIMPLE",
+          version: "1.0.0",
+        },
+        wallet: {
+          visible: true,
+          customStyle: {
+            supportChains: [TaikoJolnir]
+          }
+        },
+      }}
+    >
+      <App />
+    </AuthCoreContextProvider>
   </React.StrictMode>
 )
